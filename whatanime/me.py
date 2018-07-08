ce4c43d859f00465c9b29c9a735cd2154aa7ce0c
lewbob9927 <lewbob9927@gmail.com>
2018-07-08 13:08:37 +0000

    Added status code error catching

--- whatanime/me.py
+++ whatanime/me.py
@@ -4,4 +4,9 @@ def me(token):
     endpoint="https://whatanime.ga/api/me"
     payload={"token":token}
     r = requests.get(endpoint, params=payload)
-    return(r.json())
+    if r.status_code==403:
+        return("API token invalid")
+    elif r.status_code==401:
+        return("API token missing")
+    elif r.status_code==200:
+        return(r.json())
--- whatanime/search.py
+++ whatanime/search.py
@@ -5,5 +5,13 @@ def search(api_token, image):
     params={"token":api_token}
     data={"image":image}
     r = requests.post(endpoint, params=params, data=data)
-    result=r.json()
-    return(result)
+    if r.status_code==403:
+        return("API token invalid")
+    elif r.status_code==401:
+        return("API token missing")
+    elif r.status_code==413:
+        return("The image is too large, please reduce the image size to below 1MB")
+    elif r.status_code==429:
+        return("You have exceeded your quota. Please wait and try again soon.")
+    elif r.status_code==200:
+        return(r.json())
